da794f107dc1feb9d91e7267c62841a4df507af8
Fernando Velez Mendez <233422@ids.upchiapas.edu.mx>
2026-03-06 10:03:59 -0600

    Fix: base path and router for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Si vas a usar dominio propio de Namecheap, deja '/'
-             // Si usaras la URL de github (usuario.github.io/repo), pon '/nombre-del-repo/'
+  base: '/messi_page/', 
 })
