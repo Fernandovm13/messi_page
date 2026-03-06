3f5ae7aca61ba84188eb3fe0719163b2949b11b7
Fernando Velez Mendez <233422@ids.upchiapas.edu.mx>
2026-03-06 09:50:45 -0600

    configuration deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Si vas a usar dominio propio de Namecheap, deja '/'
+             // Si usaras la URL de github (usuario.github.io/repo), pon '/nombre-del-repo/'
 })
